6403cb5857c0b06b134b5e15e72d9f088abfb410
tat.bandelikova <tat.bandelikova@gmail.com>
2023-02-07 21:42:04 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,44 +1,69 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+    '.-': 'a',
+    '-...': 'b',
+    '-.-.': 'c',
+    '-..': 'd',
+    '.': 'e',
+    '..-.': 'f',
+    '--.': 'g',
+    '....': 'h',
+    '..': 'i',
+    '.---': 'j',
+    '-.-': 'k',
+    '.-..': 'l',
+    '--': 'm',
+    '-.': 'n',
+    '---': 'o',
+    '.--.': 'p',
+    '--.-': 'q',
+    '.-.': 'r',
+    '...': 's',
+    '-': 't',
+    '..-': 'u',
+    '...-': 'v',
+    '.--': 'w',
+    '-..-': 'x',
+    '-.--': 'y',
+    '--..': 'z',
+    '.----': '1',
+    '..---': '2',
+    '...--': '3',
+    '....-': '4',
+    '.....': '5',
+    '-....': '6',
+    '--...': '7',
+    '---..': '8',
+    '----.': '9',
+    '-----': '0',
 };
 
 function decode(expr) {
-    // write your solution here
+    const inputArr = (function (str) {
+        let arr = [];
+        for (let i = 0; i < expr.length; i += 10) {
+            let letter = [];
+            let x = expr.slice(i, i + 10);
+            for (let j = 0; j < 10; j += 2) {
+                let y = x.slice(j, j + 2);
+                y !== '00' ? letter.push(y) : null;
+            }
+            arr.push(letter);
+        }
+        return arr;
+    })(expr);
+
+    let keys = Object.entries(MORSE_TABLE);
+    let outputArr = inputArr.map((el) => {
+        let arr = keys.find(key => key[0] == makeDashDot(el));
+        return arr == undefined ? ' ' : arr[1];
+    });
+
+    return outputArr.join('');
+
+    function makeDashDot(arr) {
+        if (arr[0] == '**') return ' ';
+        return arr.map((el) => el == '10' ? '.' : '-').join('');
+    }
 }
 
 module.exports = {
